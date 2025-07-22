0fa1a40fdb91341660a35309011d1212a8aaac67
Dan Pock <self@danpock.me>
2025-07-22 10:45:15 -0400

    Adjust codegen location

--- tools.go
+++ tools.go
@@ -1,4 +1,4 @@
-//go:generate go run pkg/codegen/cleanup/main.go
-//go:generate go run pkg/codegen/main.go
+//go:generate go run internal/codegen/cleanup/main.go
+//go:generate go run internal/codegen/main.go
 //go:generate scripts/build-crds
 package main
