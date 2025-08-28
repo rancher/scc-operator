486f716d62090187ced595a11dda15cc6235845b
Dan Pock <self@danpock.me>
2025-08-28 16:54:58 -0400

    Add tests only method for overriding contexthelpers

---
+++ internal/consts/sccurls_test.go
@@ -0,0 +1,76 @@
+//go:build test
+// +build test
+
+package consts
+
+import (
+	"testing"
+
+	"github.com/rancher/scc-operator/internal/initializer"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSCCEnvironment_String(t *testing.T) {
+	asserts := assert.New(t)
+
+	var tests = []struct {
+		name  string
+		input SCCEnvironment
+		want  string
+	}{
+		{"Prod Env", Production, "production"},
+		{"Staging Env", Staging, "staging"},
+		{"PAYG Env", PayAsYouGo, "payAsYouGo"},
+		{"RGS Env", RGS, "rgs"},
+		{"unknown", 42, "unknown"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			asserts.Equal(tt.want, tt.input.String())
+		})
+	}
+}
+
+func TestAlternativeSccURLs_Ptr(t *testing.T) {
+	asserts := assert.New(t)
+
+	var tests = []struct {
+		name  string
+		input AlternativeSccURLs
+		want  string
+	}{
+		{"Prod URL", ProdSccURL, "https://scc.suse.com"},
+		{"Staging URL", StagingSccURL, "https://stgscc.suse.com"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			asserts.Equal(tt.want, *tt.input.Ptr())
+		})
+	}
+}
+
+func TestGetSCCEnvironment_Dev(t *testing.T) {
+	asserts := assert.New(t)
+	initializer.DevMode.SetForTest(true)
+	asserts.Equal(Staging, GetSCCEnvironment())
+}
+
+func TestGetSCCEnvironment_Prod(t *testing.T) {
+	asserts := assert.New(t)
+	initializer.DevMode.SetForTest(false)
+	asserts.Equal(Production, GetSCCEnvironment())
+}
+
+func TestBaseURLForSCC_Dev(t *testing.T) {
+	asserts := assert.New(t)
+	initializer.DevMode.SetForTest(true)
+	asserts.Equal(string(StagingSccURL), BaseURLForSCC())
+}
+
+func TestBaseURLForSCC_Prod(t *testing.T) {
+	asserts := assert.New(t)
+	initializer.DevMode.SetForTest(false)
+	asserts.Equal(string(ProdSccURL), BaseURLForSCC())
+}
