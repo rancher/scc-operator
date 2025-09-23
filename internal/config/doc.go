0b88895c4b8174ee31946f7a737d1c92998afe6b
Dan Pock <self@danpock.me>
2025-09-23 16:56:24 -0400

    Introduce new config package & Env defs

---
+++ internal/config/doc.go
@@ -0,0 +1,6 @@
+/*
+Package config implements various config related concerns into a single domain.
+
+This allows us to uniformly define and handle Env vars, flags and ConfigMap based config values.
+*/
+package config
