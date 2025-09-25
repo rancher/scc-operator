ffb34211b910a240defbf5a43aa69812ef083d1a
Dan Pock <self@danpock.me>
2025-09-25 18:27:01 -0400

    Add product identification package

---
+++ internal/suseconnect/products/products_test.go
@@ -0,0 +1,33 @@
+package products
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_ParseProductName(t *testing.T) {
+	rancherProduct := ParseProductName("rancher")
+	assert.Equal(t, ProductRancher, rancherProduct)
+	assert.Equal(t, ProductName("rancher"), rancherProduct.ProductName())
+	assert.Equal(t, "rancher", rancherProduct.String())
+
+	harvesterProduct := ParseProductName("harvester")
+	assert.Equal(t, ProductNone, harvesterProduct)
+	assert.Equal(t, ProductName(""), harvesterProduct.ProductName())
+	assert.Equal(t, "", harvesterProduct.String())
+}
+
+func Test_ParseProductName_Errors(t *testing.T) {
+	fakeProduct := SCCProduct(4)
+	assert.Equal(t, ProductName("unknown"), fakeProduct.ProductName())
+	assert.Equal(t, "", fakeProduct.String())
+}
+
+func Test_CustomProductName(t *testing.T) {
+	harvesterProductCustom := ProductName("harvester")
+	assert.Equal(t, ProductName("harvester"), harvesterProductCustom)
+
+	harvesterProduct := ParseProductName("harvester")
+	assert.NotEqual(t, harvesterProductCustom, harvesterProduct.ProductName())
+}
