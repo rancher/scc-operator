46f83ffe129d17236ecf2b5f4cea5baa25d19725
Dan Pock <self@danpock.me>
2025-10-30 15:18:30 -0400

    remove code that was refactored away

--- internal/config/option/util.go
+++ internal/config/option/util.go
@@ -1,42 +1,7 @@
 package option
 
-import (
-	"fmt"
-	"reflect"
-)
-
 func AllOptions() map[string]RegisteredOption {
 	return options
-}
-
-type Flags map[string]any
-
-func (f Flags) Get(key string) (string, bool) {
-	value, ok := f[key]
-	if !ok {
-		return "", false
-	}
-	return fmt.Sprintf("%v", value), ok
-}
-
-func AllFlags() Flags {
-	flags := make(Flags)
-
-	for _, ro := range options {
-		// Use the option's name as the key, and reflect to get the generic FlagValue
-		rv := reflect.ValueOf(ro)
-		if rv.Kind() == reflect.Pointer {
-			rv = rv.Elem()
-		}
-		if rv.IsValid() {
-			fv := rv.FieldByName("FlagValue")
-			if fv.IsValid() {
-				flags[ro.GetName()] = fv.Interface()
-			}
-		}
-	}
-
-	return flags
 }
 
 type EnvVarsMap map[string]string
--- internal/config/option/util_test.go
+++ internal/config/option/util_test.go
@@ -6,64 +6,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestFlags_Get(t *testing.T) {
-	flags := Flags{
-		"string-key": "a-string",
-		"bool-key":   true,
-		"int-key":    123,
-	}
-
-	t.Run("get string value", func(t *testing.T) {
-		val, ok := flags.Get("string-key")
-		assert.True(t, ok)
-		assert.Equal(t, "a-string", val)
-	})
-
-	t.Run("get bool value", func(t *testing.T) {
-		val, ok := flags.Get("bool-key")
-		assert.True(t, ok)
-		assert.Equal(t, "true", val)
-	})
-
-	t.Run("get int value", func(t *testing.T) {
-		val, ok := flags.Get("int-key")
-		assert.True(t, ok)
-		assert.Equal(t, "123", val)
-	})
-
-	t.Run("get missing value", func(t *testing.T) {
-		val, ok := flags.Get("missing-key")
-		assert.False(t, ok)
-		assert.Equal(t, "", val)
-	})
-}
-
-func Test_AllFlags_Basic(t *testing.T) {
-	// reset global registry
-	options = make(map[string]RegisteredOption)
-
-	// create options of different types
-	strOpt := NewOption("string-opt", "default")
-	intOpt := NewOption("int-opt", 42)
-	boolOpt := NewOption("bool-opt", true)
-
-	// set their FlagValue explicitly (simulating parsed flags)
-	strOpt.FlagValue = "from-flag"
-	intOpt.FlagValue = 7
-	boolOpt.FlagValue = false
-
-	flags := AllFlags()
-
-	assert.Equal(t, 3, len(flags))
-	assert.Equal(t, "from-flag", flags["string-opt"])
-	assert.Equal(t, 7, flags["int-opt"])
-	assert.Equal(t, false, flags["bool-opt"])
-
-	// cleanup
-	options = make(map[string]RegisteredOption)
-	assert.Equal(t, 0, len(options))
-}
-
 func Test_AllEnvValues_Basic(t *testing.T) {
 	// reset global registry
 	options = make(map[string]RegisteredOption)
